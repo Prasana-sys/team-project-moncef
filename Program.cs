58dfe10de604d7193aa9f795e3bf8f10d196d6bd
Prasana Ganesh <77466443+Prasana-sys@users.noreply.github.com>
2023-03-27 10:25:53 -0400

    ms Graph API setup

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using graph;
+
+namespace MonCal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var settings = Settings.LoadSettings();
+
+            // Initialize Graph
+            graph.msgraph.InitializeGraph(settings);
+        }
+    }
+}
---
+++ msgraph_api.cs
@@ -0,0 +1,15 @@
+namespace graph
+{
+    public class msgraph
+    {
+        public static void InitializeGraph(Settings settings)
+        {
+            // TODO
+        }
+
+        public async Task GreetUserAsync()
+        {
+            // TODO
+        }
+    }
+}

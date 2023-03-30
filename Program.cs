b6710a0c63ea6c76eb7bf6dba600f8a056a73376
Prasana Ganesh <77466443+Prasana-sys@users.noreply.github.com>
2023-03-29 23:21:31 -0400

    Authenticates interactively via default browser
    Previously used device code flow

--- Program.cs
+++ Program.cs
@@ -20,8 +20,69 @@ namespace MonCal
                 Console.WriteLine ("Press 1 to add test event to calendar.");
 
                 if ("1" == Console.ReadLine())
-                {
-                    await MSgraph.CreateEventAsync(test_mode);
+                {   
+                    string test_subject = "Test";
+                    var test_itemBody = new ItemBody
+                        {
+                            ContentType = BodyType.Html,
+                            Content = "This is a test.",
+                        };
+                    var test_start = new DateTimeTimeZone
+                        {
+                            DateTime = "2023-03-30T12:30:00",
+                            TimeZone = "Eastern Standard Time",
+                        };
+                    var test_end = new DateTimeTimeZone
+                        {
+                            DateTime = "2023-03-30T13:50:00",
+                            TimeZone = "Eastern Standard Time",
+                        };
+                    var test_Location = new Location
+                        {
+                            DisplayName = "SWIFT 500",
+                        };
+                    var test_Attendees = new List<Attendee>
+                        {
+                            new Attendee
+                            {
+                                EmailAddress = new EmailAddress
+                                {
+                                    Address = "khairyha@mail.uc.edu",
+                                    Name = "Hamza Khairy",
+                                },
+                                Type = AttendeeType.Required,
+                            },
+                        };
+                    var test_Recurrence = new PatternedRecurrence
+                    {
+                        Pattern = new RecurrencePattern
+                        {
+                            Type = RecurrencePatternType.Weekly,
+                            Interval = 1,
+                            DaysOfWeek = new List<DayOfWeekObject?>
+                            {
+                                DayOfWeekObject.Thursday,
+                            },
+                        },
+                        Range = new RecurrenceRange
+                        {
+                            Type = RecurrenceRangeType.EndDate,
+                            StartDate = new Microsoft.Kiota.Abstractions.Date(DateTime.Parse("2023-03-30")),
+                            EndDate = new Microsoft.Kiota.Abstractions.Date(DateTime.Parse("2023-04-28")),
+                        },
+                    };
+                    var test_AllowNewTimeProposals = true;
+                    string test_prefferedTimeZone = "Eastern Standard Time";
+                    bool test_isAllDay = false;
+                    bool test_isReminderOn = true;
+                    Int32 test_reminderMinutesBeforeStart = 15;
+                    await MSgraph.CreateEventAsync(test_subject, test_itemBody, test_start, test_end, test_Location, 
+                                                    test_Attendees, test_Recurrence, test_prefferedTimeZone, test_AllowNewTimeProposals, 
+                                                    test_isAllDay, test_isReminderOn, test_reminderMinutesBeforeStart
+                                                  );
+
+                    Console.WriteLine("Press enter to close.");
+                    Console.ReadLine();
                 }
             }
         }
